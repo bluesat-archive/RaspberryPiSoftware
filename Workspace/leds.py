b26fce7d4271d97a9ef71998a8431b59e5cbc704
axura <pi@BLUEsatPi.(none)>
2014-03-01 06:27:14 +0000

    added comment for leds.py

--- Workspace/leds.py
+++ Workspace/leds.py
@@ -5,17 +5,21 @@ RPIO.setup(2, RPIO.OUT)
 RPIO.setup(3, RPIO.OUT)
 RPIO.setup(4, RPIO.OUT)
 
-for i in [2, 3, 4]:
+for i in [2,3,4]:
 	RPIO.output(i, True)
+#comment
 
+try:
+      while True:
+	      for i in [2,3,4]:
+		      print "turning on GPIO{number}".format(number=i)
+		      RPIO.output(i, False)
+		      time.sleep(1)
+		      RPIO.output(i, True)
 
-while True:
-	for i in [2, 3, 4]:
-		print "turning on GPIO{number}".format(number=i)
-		RPIO.output(i, False)
-		time.sleep(1)
-		RPIO.output(i, True)
-
-RPIO.cleanup()
+except KeyboardInterrupt:   
+      for i in [2,3,4]:
+            RPIO.output(i,True)
+      RPIO.cleanup()
 
 
