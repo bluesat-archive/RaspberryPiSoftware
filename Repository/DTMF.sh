d5b7950f7a932cdbfae2760cf7a378352233cf10
axura <jim.lyndon.gray@gmail.com>
2014-09-21 06:32:34 +0000

    cleaned old files

--- Repository/DTMF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo rtl_fm -f $1 -M fm -s 260k -r 22050 -g 15 | multimon-ng -t raw /dev/stdin -a DTMF 
-# >(aplay -r 22050 -f S16_LE -c 1 -q -V mono -D sysdefault:CARD=ALSA) 
-#| multimon -t raw /dev/stdin -a DTMF
-# | python killCommand1.py
--- Repository/namuruLog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Logging"
-sudo minicom -b 115200 -D /dev/ttyUSB0 -C /home/data6.txt
